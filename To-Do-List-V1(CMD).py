a3f1efd6f02763167aa57faa677448f6faf3baa6
trqwaa <kamnevtima69@gmail.com>
2025-10-24 23:17:21 +0200

    You need to finish the (Delete task) tab. There's a problem with deleting an object; it's deleted exactly after you've entered its index the same number of times.

--- To-Do-List-V1(CMD).py
+++ To-Do-List-V1(CMD).py
@@ -1,30 +1,69 @@
-#Hi! I've started developing my new project called "TO-DO-LIST."
-
-#This is a list project where you can record and save the data you need. You can view the list, add items to it, delete, or exit the application, and all the data will remain saved!
-
-#In this project, I want to apply my basic Python knowledge and learn how to work with more diverse tools and libraries available in Python.
-
-#Let's start with version V-1!!!!!
 
 #_________________________________________________________________________________________________________________________________________________________________________________________________
-#list_user = []
 
+#============LISTS============
 menu = {
     1: "Add task",
     2: "Show tasks",
     3: "Delete task",
     4: "Exit"
 }
+user_list = []
+#_____________________________
+
+#============Menu(Ready V-1)============
 
 while True:
+    
     print('========== Welcome to the TO-DO-LIST menu ==========')
     for key, value in menu.items():
         print(f"{key}. {value}")
 
-    user = input("Let's make your chouse: " )
-    if user == 1:
-        print
+    menu_input = input("Let's make your chouse: " )
 
+#_____________________________
+
+#============Add task(Ready V-1)============
+
+    if menu_input == '1':
+        print('========== Add task ==========')
+
+        add_task_input = input("Let's write down the list: ")
+        user_list.append(add_task_input)
+        print( add_task_input + " was added")
+
+#_____________________________
+
+#============Show tasks(Ready V-1)============
+    elif menu_input == '2':
+
+        print('========== Show tasks ==========')
+
+        print(user_list)
+
+        print('________________________________')
+
+#_____________________________
+
+#============Delete task(NEED TO WORK)============
+
+    elif menu_input == '3':
+        print('========== Delete task ==========')
+        for x, worts in enumerate(user_list, start=1):
+            delete_task_user = int(input("Select a number to delete|" + str(user_list) + ": " ))
+            if delete_task_user == x:
+                user_list.remove(worts)
+                print("was removed " + "'" + worts + "'")
+        print("your list at the moment: " + ", ".join(map(str, user_list)))
+
+ #_____________________________  
+ 
+#============Exit(is being created)============
+
+    elif menu_input == '4':
+        None
+
+#_____________________________
 
 
 
